c69de8d1e21de44db053ac1fb371082f715b982d
aisensiy <aisensiy@163.com>
2018-08-11 15:58:58 +0800

    feat: add 80 443 port to security group

--- main.tf
+++ main.tf
@@ -62,6 +62,20 @@ resource "aws_security_group" "kubernetes" {
   }
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
